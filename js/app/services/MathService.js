ea70ca9e0cf4a8f37c42b798d25edd6f93d68714
Scotty Runyan <runyans7@gmail.com>
2016-03-16 22:08:53 -0400

    Done.

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,15 @@
 // Your code here!
+function MathService() {
+  this.sum = function(array) {
+    total = 0;
+
+    for (var i = 0; i < array.length; i++) {
+      total += array[i];
+    }
+    return total;
+  }
+}
+
+angular
+  .module('app')
+  .service('MathService', MathService);
