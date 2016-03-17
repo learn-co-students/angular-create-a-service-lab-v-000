0e5280720e316637a37cc32b3fc9de3e631e89c0
Scotty Runyan <runyans7@gmail.com>
2016-03-16 22:11:02 -0400

    Refactor using reduce

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,12 +1,7 @@
 // Your code here!
 function MathService() {
   this.sum = function(array) {
-    total = 0;
-
-    for (var i = 0; i < array.length; i++) {
-      total += array[i];
-    }
-    return total;
+    return array.reduce( (prev, curr) => prev + curr );
   }
 }
 
