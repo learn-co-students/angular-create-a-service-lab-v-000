9551243e8173d2b163428c6ba8473fb9edd75963
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-03-19 14:20:14 -0400

    Add MathService

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,14 @@
 // Your code here!
+function MathService(){
+  this.sum = function(arrayOfNums){
+    var total = 0;
+    for(var i=0; i < arrayOfNums.length ; i++){
+      total = total + arrayOfNums[i];
+    }
+    return total;
+  }
+}
+
+angular
+  .module('app')
+  .service('MathService', MathService)
