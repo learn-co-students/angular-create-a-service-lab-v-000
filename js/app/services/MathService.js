92792ae86165a069ff1b0a48a85f785672a8cf35
Chen Yang <yangc5@mail.uc.edu>
2016-03-19 16:19:13 +0000

    done

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,12 @@
 // Your code here!
+function MathService(){
+  this.sum = function(array){
+    var sum = 0;
+    array.forEach(function(i){
+      sum+=i;
+    });
+    return sum;
+  }
+}
+
+angular.module('app').service('MathService', MathService);
