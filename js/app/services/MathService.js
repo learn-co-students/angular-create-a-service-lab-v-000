33da6ee257d513a7ce716afc31028bec184b7e5b
github username <mendelblesofsky@gmail.com>
2016-03-22 10:24:18 -0400

    Done

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,12 @@
 // Your code here!
+function MathService() {
+	this.sum = function(arr) {
+		return arr.reduce(function(sum, i){
+			return sum + i;
+		});
+	}
+}
+
+angular
+	.module('app')
+	.service('MathService', MathService);
