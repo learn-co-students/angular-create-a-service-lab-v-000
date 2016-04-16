374592a09c87bb4cf937db8093a2e7d50d378398
zachnewburgh <zach.newburgh@gmail.com>
2016-04-16 12:58:02 -0400

    Done.

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,14 @@
 // Your code here!
+function MathService() {
+  this.sum = function(array) {
+    var total = array.reduce(add, 0);
+    function add(a,b) {
+      return a + b;
+    }
+    return total;
+  }
+}
+
+angular
+  .module('app')
+  .service('MathService', MathService);
