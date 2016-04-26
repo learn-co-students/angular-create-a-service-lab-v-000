a59274aef7c432165b6dd4d29440c954be6a0306
NStephenson <nickstephenson90@gmail.com>
2016-04-26 16:35:34 -0400

    made test pass

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,14 @@
 // Your code here!
+function MathService(){
+
+  this.sum = function(numbers){
+    return numbers.reduce(function(previousValue, currentValue) {
+      return previousValue + currentValue;
+    });
+  }
+
+}
+
+angular
+  .module('app')
+  .service("MathService", MathService)
