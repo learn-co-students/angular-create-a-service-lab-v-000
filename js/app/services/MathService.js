ad49948e6aad302d9fd3da7567379df2dc76fef8
Brett <brettrheenan@gmail.com>
2016-05-04 20:20:21 +0200

    Done.

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,16 @@
-// Your code here!
+function MathService() {
+	this.sum = function(numbers) {
+		var sum = 0
+		numbers.forEach(function(number) {
+			// make sure item in array is of type number so as not to throw error
+			if (typeof(number) === "number") {
+				sum += number;
+			}
+		});
+		return sum;
+	}
+}
+
+angular
+	.module('app')
+	.service('MathService', MathService)
