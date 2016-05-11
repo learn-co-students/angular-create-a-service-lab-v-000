67f2def837438ca888e29fae68a89ecfe0ddfd74
MitulMistry <MitulMistryDev@gmail.com>
2016-05-11 12:18:23 -0500

    Done.

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,14 @@
 // Your code here!
+function MathService(){
+  this.sum = function(array){
+    var total = 0;
+    for(var i = 0; i < array.length; i++){
+      total += array[i];
+    }
+    return total;
+  };
+}
+
+angular
+  .module('app')
+  .service('MathService', MathService);
