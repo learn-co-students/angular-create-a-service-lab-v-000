063b62fc8e6d2d3d0a539431fb445bce2034fbe5
jsgoldb <jgoldb@yahoo.com>
2016-05-18 09:31:52 -0500

    Done.

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,10 @@
 // Your code here!
+function MathService (){
+  this.sum = function(numArray){
+    return numArray.reduce((a, b) => a + b, 0);
+  }
+}
+
+angular
+  .module('app')
+  .service('MathService', MathService)
