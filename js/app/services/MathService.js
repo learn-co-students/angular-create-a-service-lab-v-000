788c0d0a042b6ff9b4fa921c6d9ab2f837c0f1d0
authorbeard <msnolan@gmail.com>
2016-05-20 13:09:09 -0400

    test passing

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,12 @@
-// Your code here!
+function MathService() {
+
+  this.sum = function(input){
+    return input.reduce(function(a,b){
+        return a + b
+      })
+  }
+}
+
+angular
+  .module('app')
+  .service('MathService', MathService)
