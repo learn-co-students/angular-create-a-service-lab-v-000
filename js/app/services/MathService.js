44aa28bc2f1601092de7014be6e15d101a75cdf3
cernanb <cernanb@gmail.com>
2016-05-26 17:45:45 -0700

    wrote a service to be used by the controller

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,16 @@
 // Your code here!
+function MathService(){
+  this.sum = function(array){
+    var sum = 0;
+    // console.log(array);
+    array.forEach(function(num){
+      sum += num
+    });
+    return sum;
+  };
+}
+
+
+angular
+  .module('app')
+  .service('MathService', MathService);
