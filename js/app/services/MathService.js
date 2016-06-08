9a0081ad5628d931c20413a5717039dee104ccea
coltonstaab1 <colton.staab@gmail.com>
2016-06-08 14:51:01 -0400

    Done.

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,11 @@
-// Your code here!
+function MathService() {
+  this.sum = function (arr) {
+    return arr.reduce(function (prev, next) {
+      return prev + next;
+    })
+  };
+}
+
+angular
+  .module('app')
+  .service('MathService', MathService);
