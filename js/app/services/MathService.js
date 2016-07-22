2c3b67238ca2cde0d492274b0c0abc238cc9e006
McN33ly <rmcneely23@gmail.com>
2016-07-22 14:06:00 -0400

    Done.

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,10 @@
 // Your code here!
+function MathService(){
+  this.sum = function(array){
+    return parseInt(array[0]) + parseInt(array[1])
+  };
+};
+
+angular
+  .module('app')
+  .service('MathService', MathService);
