daa11d1ee3992c38d816164a45270b3f3d5abe8d
Nathaniel Miller <dukeoflaser@gmail.com>
2016-10-06 21:00:04 -0700

    Done.

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,19 @@
 // Your code here!
+
+function MathService() {
+  console.log('This got hit.');
+  this.sum = function(numbers) {
+    var total = 0;
+
+    numbers.forEach(function(n) {
+      total += n;
+    });
+
+    return total;
+
+  }
+}
+
+angular
+  .module('app')
+  .service('MathService', MathService);
