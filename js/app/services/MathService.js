c89cd823f5941f7ff433c0603b807edefc8bbe15
michaelsimon <michael.simon@gmail.com>
2016-10-22 18:37:50 -0400

    Done.

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,12 @@
-// Your code here!
+function MathService() {
+  this.sum = function(nums) {
+    var sum = 0
+    for (var i = 0; i < nums.length; i++) {
+      sum += nums[i];
+    }
+    return sum;
+  };
+}
+angular
+  .module('app')
+  .service('MathService', MathService);
