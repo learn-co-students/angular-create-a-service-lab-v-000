11592cb5d5301f263eb18a096271d98e0bfea92f
github username <github email address>
2016-11-18 15:14:28 -0500

    Done.

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,16 @@
-// Your code here!
+function MathService() {
+
+   this.sum = function(args) {
+    var count = 0;
+    for (var i=0; i < args.length; i++) {
+      count += args[i]
+    }
+    return count;
+  }
+
+}
+
+
+angular
+  .module('app')
+  .service('MathService', MathService);
