a370ff95fe62435515867ed6a89b325d50b77ff1
Adrian Dave <adrian@paradoxlogic.co>
2016-11-30 00:57:07 -0500

    Done.

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,11 @@
-// Your code here!
+function MathService() {
+    this.sum = function(numberArray) {
+      return numberArray.reduce(function (a, b) {
+        return a + b;
+      });
+    };
+}
+
+angular
+    .module('app')
+    .service('MathService', MathService);
