1a9329c351543fa8b434cdd445b9835936e3ae15
lenn hypolite <lennhydesign@gmail.com>
2016-12-10 01:00:50 -0500

    Done.

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,15 @@
 // Your code here!
+function MathService() {
+   this.sum = function(array){
+      var total =0;
+      for(let i =0; i<array.length; i++){
+        total = array[i] + total;
+      }
+      return total;
+  }
+
+}
+
+angular
+    .module('app')
+    .service('MathService', MathService);
