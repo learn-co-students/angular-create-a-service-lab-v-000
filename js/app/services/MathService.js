3075af75c5619273b87989e2b795bb1d99a7e1af
ThisIsTyDell <milleb94@gmail.com>
2016-12-12 20:19:08 -0600

    Done.

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,13 @@
-// Your code here!
+function MathService() {
+  this.sum = function(numArray) {
+    var answer = 0
+    numArray.forEach(function(num) {
+      answer = answer + num;
+    })
+    return answer
+  }
+}
+
+angular
+  .module('app')
+  .service('MathService', MathService)
